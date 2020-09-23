d34290782a23981b58abbc4be5f82d97f083943a
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2020-09-24 01:16:45 +0300

    Updates

---
+++ precache-manifest.d9862251411e02f95442b855c6ce8bf7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0377b53665d9eb570a1ddf8f9acfc78e",
+    "url": "/workTest/index.html"
+  },
+  {
+    "revision": "d2fd79d95625c22a5b55",
+    "url": "/workTest/static/css/main.7ce4fc3f.chunk.css"
+  },
+  {
+    "revision": "4954b13b45cdb7ee97a7",
+    "url": "/workTest/static/js/2.47b3e191.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/workTest/static/js/2.47b3e191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fd79d95625c22a5b55",
+    "url": "/workTest/static/js/main.10ce27bd.chunk.js"
+  },
+  {
+    "revision": "75098ec79134f76ed67e",
+    "url": "/workTest/static/js/runtime-main.751a193d.js"
+  },
+  {
+    "revision": "fae0c8540059eb2634d6c74501dc3499",
+    "url": "/workTest/static/media/btn_delete.fae0c854.svg"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/workTest/static/media/roboto-v20-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/workTest/static/media/roboto-v20-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/workTest/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/workTest/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/workTest/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/workTest/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
